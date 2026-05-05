6527a82085318523a09c35cebd44a8e0a42d59fd
harjot-jaswal-dev <harjotjaswal.dev@gmail.com>
2026-05-05 00:53:22 -0700

    added --verbose option to prompt

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ def main():
 
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="user_prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose mode")
     args = parser.parse_args()
 
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
@@ -27,9 +28,11 @@ def main():
     if response.usage_metadata is None:
         raise RuntimeError("usage_metadata was None")
 
-    print(f"User prompt: {args.user_prompt}")
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        
     print(f"Response:\n{response.text}")
 
 
