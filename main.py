b73f5e0a69e341713f8490759a9e474ceb3ca2b7
harjot-jaswal-dev <harjotjaswal.dev@gmail.com>
2026-05-18 14:16:43 -0700

    Looped agent logic to run at 20 iterations max

--- main.py
+++ main.py
@@ -24,32 +24,46 @@ def main():
 
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
-    response = client.models.generate_content(model= "gemini-2.5-flash", contents=messages, config=types.GenerateContentConfig(tools=[available_functions] ,system_instruction=system_prompt))
+    for _ in range(20):
 
-    if response.usage_metadata is None:
-        raise RuntimeError("usage_metadata was None")
+        response = client.models.generate_content(model= "gemini-2.5-flash", contents=messages, config=types.GenerateContentConfig(tools=[available_functions] ,system_instruction=system_prompt))
 
-    if args.verbose:
-        print(f"User prompt: {args.user_prompt}")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        if response.candidates:
+            for i in response.candidates:
+                messages.append(i.content)
+
+        if response.usage_metadata is None:
+            raise RuntimeError("usage_metadata was None")
+
+        if args.verbose:
+            print(f"User prompt: {args.user_prompt}")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        
+        if response.function_calls:
+            function_responses = []
+            for f in response.function_calls:
+                function_call_results = call_function(f, verbose= args.verbose)
+                if not function_call_results.parts:
+                    raise Exception("function_call_result has no parts")
+                if function_call_results.parts[0].function_response is None:
+                    raise Exception("function_response is None")
+                if function_call_results.parts[0].function_response.response is None:
+                    raise Exception("response is None")
+                if args.verbose:
+                    print(f"-> {function_call_results.parts[0].function_response.response}")
+
+                function_responses.append(function_call_results.parts[0])
+
+            messages.append(types.Content(role="user", parts=function_responses))
+
+        else:
+            print(f"Response:\n{response.text}")
+            break
     
-    if response.function_calls:
-        for f in response.function_calls:
-            function_call_results = call_function(f, verbose= args.verbose)
-            if not function_call_results.parts:
-                raise Exception("function_call_result has no parts")
-            if function_call_results.parts[0].function_response is None:
-                raise Exception("function_response is None")
-            if function_call_results.parts[0].function_response.response is None:
-                raise Exception("response is None")
-            if args.verbose:
-                print(f"-> {function_call_results.parts[0].function_response.response}")
-
-
     else:
-        print(f"Response:\n{response.text}")
-
+        print("Maximum iterations reached without a final response")
+        exit(1)
 
 if __name__ == "__main__":
     main()
